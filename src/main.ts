644c7c220df1c01235eee2ad4552536e9bf77036
Dmytro Chyr <dima543439@gmail.com>
2024-08-12 17:34:43 +0300

    Fix CORS error

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,18 @@ import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
-import * as express from 'express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     credentials: true,
-    origin: [
-      'http://localhost:5173',
-      'https://profound-moxie-722b74.netlify.app',
-    ],
+    origin: ['http://localhost:5173', 'https://manashop.netlify.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     exposedHeaders: ['Content-Disposition'],
   });
 
   app.use(cookieParser());
-
-  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
 
   const config = new DocumentBuilder()
     .setTitle('Mana Back')
