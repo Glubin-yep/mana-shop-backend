e4e8c599c5952913b435b74a20713f527f5d703d
Dmytro Chyr <dima543439@gmail.com>
2024-08-14 20:07:08 +0300

    Fix Typo

--- src/users/dto/user-info.dto.ts
+++ src/users/dto/user-info.dto.ts
@@ -6,7 +6,7 @@ export class UserInfoDTO {
   firstName?: string;
   lastName?: string;
   email?: string;
-  adress?: string;
+  address?: string;
   delivery?: DeliveryStatusDTO[];
 
   constructor(user: UserEntity) {
@@ -14,7 +14,7 @@ export class UserInfoDTO {
     this.firstName = user.firstName;
     this.lastName = user.lastName;
     this.email = user.email;
-    this.adress = user.adress;
+    this.address = user.adress;
     this.delivery = user.delivery.map((delivery) => ({
       id: delivery.id,
       trackCode: delivery.trackCode,
