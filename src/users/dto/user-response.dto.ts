ffe4087d4cabf2908aa8982b9d3d74c34bcecac1
Dmytro Chyr <dima543439@gmail.com>
2024-08-16 16:49:05 +0300

    Fix type

--- src/users/dto/user-response.dto.ts
+++ src/users/dto/user-response.dto.ts
@@ -5,11 +5,13 @@ export class UserResponseDto {
   email: string;
   firstName: string;
   lastName: string;
+  address: string;
 
   constructor(user: UserEntity) {
     this.id = user.id;
     this.email = user.email;
     this.firstName = user.firstName;
     this.lastName = user.lastName;
+    this.address = user.adress;
   }
 }
